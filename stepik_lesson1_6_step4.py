fa3a16f5f4ac9f9f9100e5b79574df26deed3bc1
Альмира Мажитова <almiramazhitova@MacBook-Pro-Almira.local>
2022-05-31 18:10:35 +0500

    добавление файла - поиск, заполнение полей формы регистрации с помощью selenium

---
+++ stepik_lesson1_6_step4.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+import time 
+link = "http://suninjuly.github.io/simple_form_find_task.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+    #поиск первого поля ввода
+    input1 = browser.find_element_by_tag_name("input")
+    #передача значения в первое поле
+    input1.send_keys("Ivan")
+    #поиск второго поля ввода
+    input2 = browser.find_element_by_name("last_name")
+    #передача значения во второе поле
+    input2.send_keys("Petrov")
+    #поиск второго поля ввода
+    input3 = browser.find_element_by_class_name("city")
+    #передача значения в третье поле
+    input3.send_keys("Smolensk")
+    #поиск четвертого поля ввода
+    input4 = browser.find_element_by_id("country")
+    #передача значения в четвертое поле
+    input4.send_keys("Russia")
+    #поиск кнопки
+    button = browser.find_element_by_css_selector("button.btn")
+    #нажатие на кнопку
+    button.click()
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+
